a1b03a8227cb8a9e7423e60ac42b93c23ed8d02e
rui.ye <271880043@qq.com>
2020-04-04 16:07:21 +0800

    netty demo

---
+++ src/main/java/com/SimpleServer.java
@@ -0,0 +1,55 @@
+package com;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class SimpleServer {
+    public SimpleServer(int port) {
+        this.port = port;
+    }
+
+    private int port;
+
+    public void run() throws Exception{
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel sc) throws Exception {
+                            sc.pipeline().addLast(new SimpleServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SimpleServer(9999).run();
+    }
+
+
+}
